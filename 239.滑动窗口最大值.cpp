62d6b0cd8d195c85049a602c2e1de78e49be9244
Gummum <1194148070@qq.com>
2023-08-04 01:03:21 +0800

    8/4 1:03

--- 239.滑动窗口最大值.cpp
+++ 239.滑动窗口最大值.cpp
@@ -3,7 +3,7 @@
  * @Date: 2023-07-29 15:45
  * @LastEditTime: 2023-07-29 15:45
  * @FilePath: \leetcode\239.滑动窗口最大值.cpp
- * @Description: not pass
+ * @Description: pass
  */
 
 /*
@@ -16,19 +16,38 @@
 class Solution
 {
 public:
+//储存值的形式
     vector<int> maxSlidingWindow(vector<int> &nums, int k)
     {
+        vector<int> result;
         deque<int> deq;
         for(int i=0;i<k;i++)
         {
-            if(i!=0&&deq.back()<=)
-
+            while(!deq.empty()&&deq.back()<nums[i])
+            {
+                deq.pop_back();
+            }
+            deq.push_back(nums[i]);
         }
+        result.push_back(deq.front());
+        for(int i=k;i<nums.size();i++)
+        {
+            if(nums[i-k]==deq.front())
+                deq.pop_front();
+            while (!deq.empty()&&deq.back() < nums[i])
+            {
+                deq.pop_back();
+            }
+            deq.push_back(nums[i]);
+            result.push_back(deq.front());
+        }
+        return result;
     }
 };
 // @lc code=end
 /*
-int n = nums.size();
+//储存下标的形式
+        int n = nums.size();
         deque<int> q;
         for (int i = 0; i < k; i++)
         {
