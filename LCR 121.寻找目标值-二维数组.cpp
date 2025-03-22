e671905680608749bffb7457421a0945c488bd71
betty <1194148070@qq.com>
2025-03-23 01:11:17 +0800

    基于LeetCode插件新增剑指offer题目

---
+++ LCR 121.寻找目标值-二维数组.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=LCR 121 lang=cpp
+ * @lcpr version=30204
+ *
+ * [LCR 121] 寻找目标值 - 二维数组
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    bool findTargetIn2DPlants(vector<vector<int>>& plants, int target) {
+        
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [[2,3,6,8],[4,5,8,9],[5,9,10,12]]\n8\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[1,3,5],[2,5,7]]\n4\n
+// @lcpr case=end
+
+ */
+
